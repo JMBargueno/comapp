b743ea6f423a80beba7e63103727b0f6dce7a50f
jmbargueno <jmbarguenolopez@gmail.com>
2020-06-05 04:05:08 +0200

    Luismi, Miguel, me muero de sueño

--- Comapp/app/src/main/java/com/jmbargueno/comapp/ui/home/MyOrderRecyclerViewAdapter.kt
+++ Comapp/app/src/main/java/com/jmbargueno/comapp/ui/home/MyOrderRecyclerViewAdapter.kt
@@ -1,11 +1,14 @@
 package com.jmbargueno.comapp.ui.home
 
+import android.content.Intent
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.jmbargueno.comapp.OrderDetailActivity
 import com.jmbargueno.comapp.R
+import com.jmbargueno.comapp.common.MyApp
 import com.jmbargueno.comapp.model.Order
 
 
@@ -15,6 +18,17 @@ import com.jmbargueno.comapp.model.Order
  */
 class MyOrderRecyclerViewAdapter() : RecyclerView.Adapter<MyOrderRecyclerViewAdapter.ViewHolder>() {
     private var values: List<Order> = ArrayList()
+    private val mOnClickListener: View.OnClickListener
+
+    init {
+        mOnClickListener = View.OnClickListener { v ->
+            val item = v.tag as Order
+            var detail: Intent = Intent(MyApp.instance, OrderDetailActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            }
+            MyApp.instance.startActivity(detail)
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -26,11 +40,16 @@ class MyOrderRecyclerViewAdapter() : RecyclerView.Adapter<MyOrderRecyclerViewAda
         val item = values[position]
         holder.title.text = item.title
         holder.comment.text = item.comment
+        with(holder.mView) {
+            tag = item
+            setOnClickListener(mOnClickListener)
+        }
     }
 
     override fun getItemCount(): Int = values.size
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val mView: View = view
         val title: TextView = view.findViewById(R.id.textViewOrderFragmentTitle)
         val comment: TextView = view.findViewById(R.id.textViewOrderFragmentComments)
     }
