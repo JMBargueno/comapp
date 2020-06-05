8962d002450be8ebf497b99643a3fe91cf6f23ce
jmbargueno <jmbarguenolopez@gmail.com>
2020-06-05 19:07:37 +0200

    Added const url added to heroku

--- Comapp/app/src/main/java/com/jmbargueno/comapp/common/Constants.kt
+++ Comapp/app/src/main/java/com/jmbargueno/comapp/common/Constants.kt
@@ -5,6 +5,7 @@ class Constants {
         val SHARED_PREFERENCES_USER_ID = "user_id"
         val SHARED_PREFERENCES_COMMUNITY = "community"
         val BASE_URL = "http://10.0.2.2:1500/"
+        val BASE_URL_DEPLOYED = "https://comappapi.herokuapp.com/"
         val APP_SETTINGS_FILE = "APP_SETTINGS"
         const val SHARED_PREFERENCES_TOKEN = "token"
         const val TIMEOUT_INMILIS = 30000L
